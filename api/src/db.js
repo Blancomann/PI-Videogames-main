b4dca5089d1719c5a75818020036cad2cb0d777c
Agustín Blanco <agustinblancobel@gmail.com>
2022-11-25 16:36:18 -0300

    done1

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 // const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("../config.js");
 
 let sequelize =
@@ -11,7 +11,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: PORT,
+        port: DB_PORT,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
