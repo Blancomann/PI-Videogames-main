daf23622c28a7f46a12005c84b19becfd6d661cf
Agustín Blanco <agustinblancobel@gmail.com>
2022-11-25 02:33:31 -0300

    DONE

--- api/src/db.js
+++ api/src/db.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("../config.js");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("../config.js");
 
 let sequelize =
   process.env.NODE_ENV === "production"
