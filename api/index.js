18e7633c5e5cc84fe89aa2d00e9b797579f48b52
Agustín Blanco <agustinblancobel@gmail.com>
2022-11-25 01:56:54 -0300

    Last2

--- api/index.js
+++ api/index.js
@@ -19,9 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-// const {PORT} = require("./config.js")
+const {PORT} = require("./config.js")
 // const {PORT} = process.env;
-const PORT = 6192;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
