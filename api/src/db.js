1e7628bcdbb825b228dbb5ffc5d3b36dbbdadb28
Agustín Blanco <agustinblancobel@gmail.com>
2022-11-25 15:58:09 -0300

    DONE

--- api/src/db.js
+++ api/src/db.js
@@ -11,7 +11,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: PORT,
+        port: 7107,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
