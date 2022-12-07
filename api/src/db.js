ebfddc93187b74c269ad5e445f9a1c9d675ce96d
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-07 15:27:00 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 // const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = require("../config.js");
 
 // let sequelize =
