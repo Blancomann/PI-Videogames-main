81e9b71ff67a346e0fd22ee9c52a329a849bdb87
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-07 19:57:17 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -10,7 +10,7 @@ let sequelize =
         database: process.env.DB_NAME,
         dialect: "postgres",
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: 5432,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         pool: {
