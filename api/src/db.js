82a835f1aa99feb29cea7fc1d3c6e936dd1daf9e
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-07 15:21:32 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -34,7 +34,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 //         { logging: false, native: false }
 //       );
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
