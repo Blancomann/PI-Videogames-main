492ec62ecedb4bf694797cb3e2ff83a0c1e3cf81
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-07 17:11:59 -0300

    deploy

--- api/src/routes/genresR.js
+++ api/src/routes/genresR.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const { Router } = require('express');
 const { Genre } = require('../db.js');
-// const {API_KEY} = process.env;
+const {API_KEY} = process.env;
 // const {API_KEY} = require("../../config.js");
 
 const genreRouter = Router();
 
 genreRouter.get('/', async(req, res) => {
   try{
-    const apiData = await axios.get(`https://api.rawg.io/api/genres?key=19ffbe207bf84c358063377e656fbf27`);
+    const apiData = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.API_KEY}`);
     const genres = apiData.data.results;
     genres.forEach(async(g) => {
       await Genre.findOrCreate({
