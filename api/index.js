3cb8b869b5eb3a79ecbfb387290ffcd154931e0e
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-12 20:22:08 -0300

    LocalHost Back fixed

--- api/index.js
+++ api/index.js
@@ -23,8 +23,15 @@ const { conn } = require('./src/db.js');
 // const {DB_PORT} = process.env;
 
 // Syncing all the models at once.
+
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
+
+// conn.sync({ force: true }).then(() => {
+//   server.listen(3001, () => {
+//     console.log(`listening at 3001`); // eslint-disable-line no-console
+//   });
+// });
--- api/src/routes/videogamesR.js
+++ api/src/routes/videogamesR.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { Videogame, Genre } = require('../db.js');
 const axios = require('axios')
-// const {API_KEY} = process.env;
+const {API_KEY} = process.env;
 // const {API_KEY} = require("../../config.js");
 
 const videogamesRouter = Router();
@@ -9,6 +9,20 @@ const videogamesRouter = Router();
 insert into videogames (name, description, released, rating, platforms)
 values ('MagnoKiller', 'omosho', '12-03-2020', 100, 'todo terreno');
 */
+
+const helpFunction = (page) => {
+  let info = page.data.results;
+  return info.map((g) => {
+    return{
+      id: g.id,
+      name: g.name,
+      background_image: g.background_image,
+      rating : g.rating,
+      genres: g.genres.map((ge) => ge.name)
+    }
+  })
+}
+
 videogamesRouter.get('/', async(req, res) => {
   let dbGames = await Videogame.findAll({
     include: Genre
@@ -26,7 +40,7 @@ videogamesRouter.get('/', async(req, res) => {
   if(req.query.name){
     try{
       //busco en API
-      const apiData = await axios.get(`https://api.rawg.io/api/games?search=${req.query.name}&key=${process.env.API_KEY}`, { headers: { "Accept-Encoding": "gzip,deflate,compress" },});
+      const apiData = await axios.get(`https://api.rawg.io/api/games?search=${req.query.name}&key=${API_KEY}`, { headers: { "Accept-Encoding": "gzip,deflate,compress" },});
       if(apiData.data.count === 0){
         console.log(`Not found: ${req.query.name}`);
         return res.status(204).json(`Not found: "${req.query.name}"`);
@@ -49,23 +63,40 @@ videogamesRouter.get('/', async(req, res) => {
     }
   }else{//muestro todos los juegos
     try{
-      let pages = 0;
       let results = [...dbGames];
-      let apiData = await axios.get(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`, { headers: { "Accept-Encoding": "gzip,deflate,compress" },});
-      while(pages <= 4){
-        pages++;
-        let loadedGames = apiData.data.results.map((g) => {
-          return{
-            id: g.id,
-            name: g.name,
-            background_image: g.background_image,
-            rating : g.rating,
-            genres: g.genres.map((ge) => ge.name)
-          }
-        });
-        results = [...results, ...loadedGames];
-        apiData = await axios.get(apiData.data.next);
-      }
+//  HARDCODEAR PRIMERAS PAGINAS Y ELIMINAR BUCLES FOR Y WHILE
+      const page1 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=1', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      const page2 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=2', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      const page3 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=3', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      const page4 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=4', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      const page5 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=5', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      const page6 = await axios.get('https://api.rawg.io/api/games?key=19ffbe207bf84c358063377e656fbf27&page=6', { headers: { "Accept-Encoding": "gzip,deflate,compress" }});
+      results = [...results, ...helpFunction(page1), ...helpFunction(page2), ...helpFunction(page3), ...helpFunction(page4), ...helpFunction(page5), ...helpFunction(page6)];
+
+
+
+
+// console.log(aux);
+
+
+      // let pages = 4;
+      // let results = [...dbGames];
+      // let apiData = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`, { headers: { "Accept-Encoding": "gzip,deflate,compress" },});
+      // // while(pages <= 4){
+      //   // pages++;
+      // for(let i = 0 ; i <= pages ; i++){
+      //   let loadedGames = apiData.data.results.map((g) => {
+      //     return{
+      //       id: g.id,
+      //       name: g.name,
+      //       background_image: g.background_image,
+      //       rating : g.rating,
+      //       genres: g.genres.map((ge) => ge.name)
+      //     }
+      //   });
+      //   results = [...results, ...loadedGames];
+      //   apiData = await axios.get(apiData.data.next);
+      // }
       return res.status(200).json(results);
     }catch(e){
       console.log(e);
