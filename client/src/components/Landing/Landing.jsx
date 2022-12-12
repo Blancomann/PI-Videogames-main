a2a4211028f8d8f62df0f63ce05ec8f293b3e9dd
Agustín Blanco <agustinblancobel@gmail.com>
2022-12-12 20:49:57 -0300

    Webpack dotenv

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import wallpaper from '../../images/LandingWallpaper.png'
 import s from './landing.module.css';
 
 const Landing = () => {
   return(
     <div className={s.container}>
-      {/* <img className="wallpaper" src={wallpaper} alt="wallpaper" /> */}
       <Link to='/videogames'>
         <button className={s.btn}>PRESS START</button>
       </Link>
